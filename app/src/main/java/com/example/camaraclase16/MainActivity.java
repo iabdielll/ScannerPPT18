4ebe3fc3d0d5780f91700d2c4e3190290581f2d5
abdie <abdielbarrapino@gmail.com>
2023-12-06 23:44:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/camaraclase16/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.camaraclase16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnScan);
+        textView = findViewById(R.id.txtRes);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setPrompt("Escanea un código de barras o código QR");
+                integrator.initiateScan();
+            }
+        });
+    }
+}
